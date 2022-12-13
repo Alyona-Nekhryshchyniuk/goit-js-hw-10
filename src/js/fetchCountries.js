f181da21b5c6064f682b158bd830ab615bf21678
Alyona-Nekhryshchyniuk <o.nekhryshchyniuk@gmail.com>
2022-12-13 15:48:42 +0200

    axios download

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,11 +1,24 @@
+import axios from 'axios';
+// export const fetchCountries = name => {
+//   return fetch(
+//     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`
+//   ).then(resp => {
+//     if (resp.ok) {
+//       return resp.json();
+//     } else {
+//       throw new Error('error');
+//     }
+//   });
+// };
+
 export const fetchCountries = name => {
-  return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`
-  ).then(resp => {
-    if (resp.ok) {
-      return resp.json();
-    } else {
-      throw new Error('error');
-    }
-  });
+  return axios
+    .get(
+      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`,
+      name
+    )
+    .then(({ data }) => {
+      console.log(data);
+      return data;
+    });
 };
