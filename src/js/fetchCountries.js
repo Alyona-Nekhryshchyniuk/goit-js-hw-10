310c11cd1bcbd19d7db56fd8aa5c5b54be949392
Alyona-Nekhryshchyniuk <o.nekhryshchyniuk@gmail.com>
2022-12-13 15:10:13 +0200

    ...

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,7 @@
 export const fetchCountries = name => {
-  return fetch(`https://restcountries.com/v3.1/name/${name}`).then(resp => {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`
+  ).then(resp => {
     return resp.json();
   });
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,48 +1,53 @@
 import '../css/styles.css';
 import debounce from 'lodash.debounce';
-import fetchCountries from './fetchCountries';
+import { fetchCountries } from './fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
 
 const input = document.querySelector('input');
 const list = document.querySelector('ul');
 const DEBOUNCE_DELAY = 300;
+let type;
 
 const renderFlagAndName = (imgSrc, countryName) => {
   list.insertAdjacentHTML(
     'beforeend',
-    `<li class="list-item"> <img src="${imgSrc}" alt="${countryName} flag"> ${countryName}</li>`
+    `<li class="list-item country-name">
+    <img src="${imgSrc}" alt="${countryName} flag">
+    ${countryName}</li>`
   );
 };
-const renderCountryDetails = (cap, pop, lang) => {
+const renderCountryDetails = (capital, population, language) => {
   list.insertAdjacentHTML(
     'beforeend',
-    `<li class="list-item">  <span class="item-label">Capital:</span>  ${cap}</li><li class="list-item"> <span class="item-label">Population:</span>  ${pop}</li><li class="list-item"> <span class="item-label">Languages:</span>  ${lang}</li>`
+    `<li class="list-item"><span class="item-label">Capital:</span>${capital}</li>
+    <li class="list-item"><span class="item-label">Population:</span> ${population}</li>
+    <li class="list-item"> <span class="item-label">Languages:</span>  ${language}</li>`
   );
 };
 
 const inputHandle = () => {
-  const name = input.value;
   list.innerHTML = '';
-  fetchCountries(name)
+
+  fetchCountries(input.value.trim())
     .then(array => {
-      if (array.length === 0) {
-        console.log('Oops. There is no country with that name.');
-      } else if (array.length > 4) {
-        console.log('too many matches found');
+      if (array.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name'
+        );
       } else if (array.length === 1) {
-        array.map(el => {
-          renderFlagAndName(el.flags.png, el.name.common);
-          let langs = Object.values(el.languages);
-          renderCountryDetails(el.capital, el.population, langs);
+        array.map(({ flags, name, capital, population, languages }) => {
+          renderFlagAndName(flags.svg, name.official);
+          renderCountryDetails(capital, population, Object.values(languages));
         });
       } else {
-        array.map(el => {
-          console.log(el);
-          renderFlagAndName(el.flags.png, el.name.common);
+        array.map(({ flags, name }) => {
+          renderFlagAndName(flags.svg, name.official);
         });
       }
     })
     .catch(() => {
-      console.log('errrrrorrr');
+      Notify.failure('Oops. There is no country with that name.');
     });
 };
 
